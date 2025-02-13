2b8e224d4ce5f594a1b43c7a06908ff8a9b6a66f
yuli24of <yuli24of@student.ju.se>
2025-02-13 16:12:54 +0100

    Revert "Merge branch 'main' of https://github.com/front-end-fundamentals-2025/fef-project-group-01"
    This reverts commit 5df3a9ede8a50c7427e018bcde11103c32a9c2ae, reversing
    changes made to b68fc8fae321ecd1f37cec3442e63ba932728e8c.

--- js/checkout.js
+++
@@ -1,8 +0,0 @@
-const orderButton = document.querySelector(".order-button");
-const headerElement = document.getElementById("checkout-header");
-const formElement = document.getElementById("checkout-form");
-
-orderButton.addEventListener("click", function (event) {
-  headerElement.innerText = "Thank You for Your Order!";
-  formElement.style.display = "none";
-});
