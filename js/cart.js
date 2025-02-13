1f2b3c516069b8d975ce5214be177565dd7ba456
anjatypes <haan21cf@student.ju.se>
2025-02-13 11:46:45 +0100

    small fixes to cart

--- js/cart.js
+++ js/cart.js
@@ -4,8 +4,8 @@ let cart = JSON.parse(localStorage.getItem("cart")) || [];
 if (cart.length === 0) {
   cartContainer.innerHTML = `
   <p>Your bag is currently empty!</p>
-  <button onclick="window.location.href='/all-product-women.html'">Shop Women</button>
-   <button onclick="window.location.href='/all-product-men.html'">Shop Men</button>`;
+  <button onclick="window.location.href='all-product-women.html'">Shop Women</button>
+   <button onclick="window.location.href='all-product-men.html'">Shop Men</button>`; //open a new page learned from https://www.w3schools.com/js/js_window_location.asp
 } else {
   // code written with knowledge from lectures and with inspiration from youtube; https://www.youtube.com/watch?v=bCTd1eRX7Iw, https://www.youtube.com/watch?v=pRkHOD_nkH4
 
@@ -21,7 +21,7 @@ if (cart.length === 0) {
     cartContainer.innerHTML += itemHTML;
   });
 
-  let checkoutButton = `<button id="checkout-button" onclick="window.location.href='checkout.html'">PROCEED TO CHECKOUT</button>`; //open a new page learned from https://www.w3schools.com/js/js_window_location.asp
+  let checkoutButton = `<button id="checkout-button" onclick="window.location.href='checkout.html'">PROCEED TO CHECKOUT</button>`;
   cartContainer.innerHTML += checkoutButton;
 }
 
