be37854e032cd83faabb6c5b84eccd31b59b136d
anjatypes <haan21cf@student.ju.se>
2025-02-19 13:43:47 +0100

    logo update

--- js/checkout.js
+++ js/checkout.js
@@ -1,8 +1,11 @@
 const orderButton = document.querySelector(".order-button");
 const headerElement = document.getElementById("checkout-header");
 const formElement = document.getElementById("checkout-form");
+const footerElement = document.getElementById("checkout-footer");
 
 orderButton.addEventListener("click", function (event) {
   headerElement.innerText = "Thank You for Your Order!";
   formElement.style.display = "none";
+  footerElement.style.marginTop = "600px";
+  footerElement.style.borderTop = "2px solid black";
 });
