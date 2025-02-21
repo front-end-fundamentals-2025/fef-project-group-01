565fdb45cae53c09f74b94a351afc990e4cd1f5a
anjatypes <haan21cf@student.ju.se>
2025-02-21 10:34:28 +0100

    Update cart-page.js

--- js/cart-page.js
+++ js/cart-page.js
@@ -4,8 +4,8 @@ let cart = JSON.parse(localStorage.getItem("cart")) || [];
 if (cart.length === 0) {
   cartContainer.innerHTML = `
   <p>Your bag is currently empty!</p>
-  <button onclick="window.location.href='/all-product-women.html'">Shop Women</button>
-   <button onclick="window.location.href='/all-product-men.html'">Shop Men</button>`; //open a new page learned from https://www.w3schools.com/js/js_window_location.asp
+  <button onclick="window.location.href='all-product-women.html'">Shop Women</button>
+   <button onclick="window.location.href='all-product-men.html'">Shop Men</button>`; //open a new page learned from https://www.w3schools.com/js/js_window_location.asp
 } else {
   cart.forEach((item, index) => {
     // code written with knowledge from lectures and with inspiration from youtube; https://www.youtube.com/watch?v=bCTd1eRX7Iw, https://www.youtube.com/watch?v=pRkHOD_nkH4
